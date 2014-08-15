e0787d653ae89120e890ba2bb03537b5230f372e
Chen Zhongzheng <chenzz.jasonchen@gmail.com>
2014-08-16 00:36:46 +0800

    Add integer_to_roman.cc

--- integer_to_roman.cc
+++ integer_to_roman.cc
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -13,10 +14,10 @@ class Solution
 public:
 	string intToRoman(int num)
 	{
-		vector<string> roman= {"M", "CM", "D", "CD", "C", "XC", 
-			"L", "XL", "X", "IX", "V", "IV", "I"};
 		vector<int> integer= {1000, 900, 500, 400, 100, 90, 50, 40, 
 			10, 9, 5, 4, 1};
+		vector<string> roman= {"M", "CM", "D", "CD", "C", "XC", 
+			"L", "XL", "X", "IX", "V", "IV", "I"};
 
 		string res;
 		for (int i = 0; num != 0; )
@@ -36,9 +37,41 @@ public:
 	}
 };
 
+//Solution2.
+//	采用pair作为数组元素，逻辑上更清晰。
+class Solution2
+{
+public:
+	string intToRoman(int num)
+	{
+		vector<pair<int, string>> intRoman = {
+			{1000, "M"}, {900, "CM"}, {500, "D"}, 
+			{400, "CD"}, {100, "C"}, {90, "XC"}, 
+			{50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, 
+			{5, "V"}, {4, "IV"}, {1, "I"}
+		};
+		string res;
+
+		for (auto it = intRoman.begin(); num != 0;)
+		{
+			if (num >= it->first)
+			{
+				num -= it->first;
+				res += it->second;
+			}
+			else
+			{
+				it++;
+			}
+		}
+
+		return res;
+	}
+};
+
 int main()
 {
-	Solution s;
+	Solution2 s;
 	cout << s.intToRoman(2344);
 
 	return 0;
