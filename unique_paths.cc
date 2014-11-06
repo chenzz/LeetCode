f05cee682348618ab12395bb15786b66144fb163
Chen Zhongzheng <chenzz.jasonchen@gmail.com>
2014-11-06 19:04:08 +0800

    improve unique_paths.cc

--- unique_paths.cc
+++ unique_paths.cc
@@ -8,12 +8,7 @@ class Solution
 public:
 	int uniquePaths(int m, int n)
 	{
-		vector<vector<int>> paths(m, vector<int>(n));
-
-		for (int i = 0; i < m; i++)
-			paths[i][0] = 1;
-		for (int i = 0; i < n; i++)
-			paths[0][i] = 1;
+		vector<vector<int>> paths(m, vector<int>(n, 1));
 
 		for (int i = 1; i < m; i++)
 			for (int j = 1; j < n; j++)
